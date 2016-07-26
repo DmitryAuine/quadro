2000758d03e79eb6a281890f1cdd87fbf782fae7
Dmitry <DmitryAuine@users.noreply.github.com>
2016-07-26 22:31:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var readlineSync = require('readline-sync');
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
+
 var board = new five.Board({
   io: new Raspi()
 });
@@ -11,33 +12,34 @@ board.on("ready", function() {
   });
 
   imu.on("change", function() {
-    console.log(`
-      [TEMPERATURE]
-      celsius      : ${this.temperature.celsius}
-      fahrenheit   : ${this.temperature.fahrenheit}
-      kelvin       : ${this.temperature.kelvin}
-      --------------------------------------
-    
-      [ACCELEROMETER]
-      x            : ${this.accelerometer.x}
-      y            : ${this.accelerometer.y}
-      z            : ${this.accelerometer.z}
-      pitch        : ${this.accelerometer.pitch}
-      roll         : ${this.accelerometer.roll}
-      acceleration : ${this.accelerometer.acceleration}
-      inclination  : ${this.accelerometer.inclination}
-      orientation  : ${this.accelerometer.orientation}
-      --------------------------------------
-    
-      [GYRO]
-      x            : ${this.gyro.x}
-      y            : ${this.gyro.y}
-      z            : ${this.gyro.z}
-      pitch        : ${this.gyro.pitch}
-      roll         : ${this.gyro.roll}
-      yaw          : ${this.gyro.yaw}
-      rate         : ${this.gyro.rate}
-      isCalibrated : ${this.gyro.isCalibrated}
-      --------------------------------------
-  `);
+      console.log(`
+        [TEMPERATURE]
+        celsius      : ${this.temperature.celsius}
+        fahrenheit   : ${this.temperature.fahrenheit}
+        kelvin       : ${this.temperature.kelvin}
+        --------------------------------------
+      
+        [ACCELEROMETER]
+        x            : ${this.accelerometer.x}
+        y            : ${this.accelerometer.y}
+        z            : ${this.accelerometer.z}
+        pitch        : ${this.accelerometer.pitch}
+        roll         : ${this.accelerometer.roll}
+        acceleration : ${this.accelerometer.acceleration}
+        inclination  : ${this.accelerometer.inclination}
+        orientation  : ${this.accelerometer.orientation}
+        --------------------------------------
+      
+        [GYRO]
+        x            : ${this.gyro.x}
+        y            : ${this.gyro.y}
+        z            : ${this.gyro.z}
+        pitch        : ${this.gyro.pitch}
+        roll         : ${this.gyro.roll}
+        yaw          : ${this.gyro.yaw}
+        rate         : ${this.gyro.rate}
+        isCalibrated : ${this.gyro.isCalibrated}
+        --------------------------------------
+    `);
+  });
 });
