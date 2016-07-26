6d28356cfdd9b7c21c3cc79560c2f822b58e3152
Dmitry <DmitryAuine@users.noreply.github.com>
2016-07-26 22:43:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,34 +12,32 @@ board.on("ready", function() {
   });
 
   imu.on("change", function() {
-      console.log(`\x1B[1A\x1B[K
-        [TEMPERATURE]
-        celsius      : ${this.temperature.celsius}
-        fahrenheit   : ${this.temperature.fahrenheit}
-        kelvin       : ${this.temperature.kelvin}
-        --------------------------------------
-      
-        [ACCELEROMETER]
-        x            : ${this.accelerometer.x}
-        y            : ${this.accelerometer.y}
-        z            : ${this.accelerometer.z}
-        pitch        : ${this.accelerometer.pitch}
-        roll         : ${this.accelerometer.roll}
-        acceleration : ${this.accelerometer.acceleration}
-        inclination  : ${this.accelerometer.inclination}
-        orientation  : ${this.accelerometer.orientation}
-        --------------------------------------
-      
-        [GYRO]
-        x            : ${this.gyro.x}
-        y            : ${this.gyro.y}
-        z            : ${this.gyro.z}
-        pitch        : ${this.gyro.pitch}
-        roll         : ${this.gyro.roll}
-        yaw          : ${this.gyro.yaw}
-        rate         : ${this.gyro.rate}
-        isCalibrated : ${this.gyro.isCalibrated}
-        --------------------------------------
-    `);
+    console.log("temperature", '\x1b[0G');
+    console.log("  celsius      : ", this.temperature.celsius, '\x1b[0G');
+    console.log("  fahrenheit   : ", this.temperature.fahrenheit, '\x1b[0G');
+    console.log("  kelvin       : ", this.temperature.kelvin, '\x1b[0G');
+    console.log("--------------------------------------", '\x1b[0G');
+  
+    console.log("accelerometer", '\x1b[0G');
+    console.log("  x            : ", this.accelerometer.x, '\x1b[0G');
+    console.log("  y            : ", this.accelerometer.y, '\x1b[0G');
+    console.log("  z            : ", this.accelerometer.z, '\x1b[0G');
+    console.log("  pitch        : ", this.accelerometer.pitch, '\x1b[0G');
+    console.log("  roll         : ", this.accelerometer.roll, '\x1b[0G');
+    console.log("  acceleration : ", this.accelerometer.acceleration, '\x1b[0G');
+    console.log("  inclination  : ", this.accelerometer.inclination, '\x1b[0G');
+    console.log("  orientation  : ", this.accelerometer.orientation, '\x1b[0G');
+    console.log("--------------------------------------", '\x1b[0G');
+  
+    console.log("gyro", '\x1b[0G');
+    console.log("  x            : ", this.gyro.x, '\x1b[0G');
+    console.log("  y            : ", this.gyro.y, '\x1b[0G');
+    console.log("  z            : ", this.gyro.z, '\x1b[0G');
+    console.log("  pitch        : ", this.gyro.pitch, '\x1b[0G');
+    console.log("  roll         : ", this.gyro.roll, '\x1b[0G');
+    console.log("  yaw          : ", this.gyro.yaw, '\x1b[0G');
+    console.log("  rate         : ", this.gyro.rate, '\x1b[0G');
+    console.log("  isCalibrated : ", this.gyro.isCalibrated, '\x1b[0G');
+    console.log("--------------------------------------");
   });
 });
