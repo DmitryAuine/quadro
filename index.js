48e6c6939fe7d322f5ba26b130ec8f43e71de17f
Dmitry <DmitryAuine@users.noreply.github.com>
2016-07-27 08:30:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
+
 var board = new five.Board({
   io: new Raspi()
 });
@@ -7,12 +8,23 @@ var socketUser = null;
 
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
+var fs = require('fs');
+
 app.listen(80);
 
-function handler(req, res){
-  res.writeHead(200);
-  res.end();
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
 }
+
 
 io.on('connection', function (socket) {
   socketUser = socket;
